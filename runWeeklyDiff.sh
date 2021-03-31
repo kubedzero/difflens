3a9628f438c68b8ace3cfe8c5462a37a00cdbc96
KZ <kubedzero@gmail.com>
2021-03-30 19:27:54 -0700

    Run Beautysh on the shell script for standardized formatting

--- runWeeklyDiff.sh
+++ runWeeklyDiff.sh
@@ -31,8 +31,8 @@ screen_path="/usr/bin/screen"
 python3_path="/usr/bin/python3"
 pip3_path="/usr/bin/pip3"
 if [[ -z "$screen_path" || -z "$python3_path" || -z "$pip3_path" ]]; then
-  echo "One or more of $screen_path $python3_path $pip3_path did not exist and is required for operation. Exiting"
-  exit 1
+    echo "One or more of $screen_path $python3_path $pip3_path did not exist and is required for operation. Exiting"
+    exit 1
 fi
 
 # Use pip3 to handle dependency install
@@ -46,39 +46,39 @@ $pip3_path install -r $project_dir/requirements.txt --no-index --find-links file
 echo "Assembling input arguments and starting screens for each disk"
 # https://stackoverflow.com/questions/169511
 for disk_num in $(seq 1 $max_disk_num); do
-  # Construct the root of our output files. All outputs will share this prefix and path
-  output_file_root="$output_dir/$run_date-disk$disk_num"
-  # Construct the path where we'll store the full scan's hash list
-  output_hash_file="$output_file_root-hashes$file_suffix"
-  # Construct the path where we'll store the removed files list
-  output_removed_files="$output_file_root-removed$file_suffix"
-  # Construct the path where we'll store the added files list
-  output_added_files="$output_file_root-added$file_suffix"
-  # Construct the path where we'll store the modified files list
-  output_modified_files="$output_file_root-modified$file_suffix"
-  # Construct the path where we'll store the duplicate files list
-  output_duplicates="$output_file_root-duplicates$file_suffix"
+    # Construct the root of our output files. All outputs will share this prefix and path
+    output_file_root="$output_dir/$run_date-disk$disk_num"
+    # Construct the path where we'll store the full scan's hash list
+    output_hash_file="$output_file_root-hashes$file_suffix"
+    # Construct the path where we'll store the removed files list
+    output_removed_files="$output_file_root-removed$file_suffix"
+    # Construct the path where we'll store the added files list
+    output_added_files="$output_file_root-added$file_suffix"
+    # Construct the path where we'll store the modified files list
+    output_modified_files="$output_file_root-modified$file_suffix"
+    # Construct the path where we'll store the duplicate files list
+    output_duplicates="$output_file_root-duplicates$file_suffix"
 
-  # Find the previous hash to diff against.
-  # Since we prefix the filenames with date, we can sort and get the last line to find the most recent file
-  # https://www.geeksforgeeks.org/mindepth-maxdepth-linux-find-command-limiting-search-specific-directory/
-  # https://superuser.com/questions/294161/unix-linux-find-and-sort-by-date-modified
-  # https://www.cyberciti.biz/faq/search-for-files-in-bash/
-  # https://www.unix.com/shell-programming-and-scripting/119458-find-most-recent-file-containing-certain-string.html
-  # https://stackoverflow.com/questions/1015678/get-most-recent-file-in-a-directory-on-linux
-  previous_hash_file_pattern="*-disk$disk_num-hashes$file_suffix"
-  previous_hash_file=$(find "$output_dir" -type f -maxdepth 1 -name "$previous_hash_file_pattern" | sort -n | tail -1)
-  # If the previous file did not exist, make a stand-in path instead of an empty string
-  # https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty/
-  [[ -z "$previous_hash_file" ]] && previous_hash_file="no_such_file"
-  echo "Previous hash file we'll compare disk$disk_num against is $previous_hash_file"
+    # Find the previous hash to diff against.
+    # Since we prefix the filenames with date, we can sort and get the last line to find the most recent file
+    # https://www.geeksforgeeks.org/mindepth-maxdepth-linux-find-command-limiting-search-specific-directory/
+    # https://superuser.com/questions/294161/unix-linux-find-and-sort-by-date-modified
+    # https://www.cyberciti.biz/faq/search-for-files-in-bash/
+    # https://www.unix.com/shell-programming-and-scripting/119458-find-most-recent-file-containing-certain-string.html
+    # https://stackoverflow.com/questions/1015678/get-most-recent-file-in-a-directory-on-linux
+    previous_hash_file_pattern="*-disk$disk_num-hashes$file_suffix"
+    previous_hash_file=$(find "$output_dir" -type f -maxdepth 1 -name "$previous_hash_file_pattern" | sort -n | tail -1)
+    # If the previous file did not exist, make a stand-in path instead of an empty string
+    # https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty/
+    [[ -z "$previous_hash_file" ]] && previous_hash_file="no_such_file"
+    echo "Previous hash file we'll compare disk$disk_num against is $previous_hash_file"
 
-  # Construct the path to the disk, which we use as the working directory
-  cd "/mnt/disk$disk_num"
+    # Construct the path to the disk, which we use as the working directory
+    cd "/mnt/disk$disk_num"
 
-  # Construct the list of input arguments
-  # https://stackoverflow.com/questions/46807924/bash-split-long-string-argument-to-multiple-lines
-  diff_lens_args="--scan_directory . \
+    # Construct the list of input arguments
+    # https://stackoverflow.com/questions/46807924/bash-split-long-string-argument-to-multiple-lines
+    diff_lens_args="--scan_directory . \
   --output_hash_file $output_hash_file \
   --comparison_hash_file $previous_hash_file \
   --output_removed_files $output_removed_files \
@@ -87,17 +87,17 @@ for disk_num in $(seq 1 $max_disk_num); do
   --output_duplicates $output_duplicates  \
   --log_update_interval_seconds 60"
 
-  # Construct the full command used to start up diff-lens. We'll trigger this with bash
-  diff_lens_command="$python3_path $project_dir/run.py $diff_lens_args"
-  # Construct the screen session name
-  screen_name="diff-lens-disk$disk_num"
+    # Construct the full command used to start up diff-lens. We'll trigger this with bash
+    diff_lens_command="$python3_path $project_dir/run.py $diff_lens_args"
+    # Construct the screen session name
+    screen_name="diff-lens-disk$disk_num"
 
-  # Run the script in a detached screen session
-  # -L required to enable logs. -Logfile to specify where. -S to name the session. -dm <cmd> to run <cmd> in screen
-  # https://superuser.com/questions/454907/how-to-execute-a-command-in-screen-and-detach
-  # https://fvdm.com/code/howto-write-screen-output-to-a-log-file
-  $screen_path -L -Logfile "$log_output_dir/$run_date-$screen_name.log" -S "$screen_name" -dm bash -c "$diff_lens_command"
-  echo -e "Monitor logs at $log_output_dir/$run_date-$screen_name.log for Screen with name $screen_name\n\n"
+    # Run the script in a detached screen session
+    # -L required to enable logs. -Logfile to specify where. -S to name the session. -dm <cmd> to run <cmd> in screen
+    # https://superuser.com/questions/454907/how-to-execute-a-command-in-screen-and-detach
+    # https://fvdm.com/code/howto-write-screen-output-to-a-log-file
+    $screen_path -L -Logfile "$log_output_dir/$run_date-$screen_name.log" -S "$screen_name" -dm bash -c "$diff_lens_command"
+    echo -e "Monitor logs at $log_output_dir/$run_date-$screen_name.log for Screen with name $screen_name\n\n"
 done
 
 echo "Screens started for $max_disk_num diff-lens executions. Use screen -r <name> to connect."

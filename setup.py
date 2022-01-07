c7d791db47b7bc494c1a8a0969b3a1ed78a11398
KZ <kubedzero@gmail.com>
2022-01-06 22:42:50 -0500

    Update the package versions in setup.py that feed the Python build process, add Pipfile.lock

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setuptools.setup(
     # are also needed when installing this .whl from a package index
     # https://medium.com/expedia-group-tech/simplifying-python-builds-74e76802444f
     # https://stackoverflow.com/questions/49496994 potential alternate methods of auto-syncing from Pipfile
-    install_requires=["blake3~=0.1.8", "pandas~=1.2.3", "psutil~=5.8.0"]
+    install_requires=["blake3~=0.2.1", "pandas~=1.3.5", "psutil~=5.9.0"]
 )
